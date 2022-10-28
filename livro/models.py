fcc493f282193a54cc47338447509f6ef24e7486
marcosfacini <marcosfacini5@gmail.com>
2022-10-27 23:24:25 -0300

    corrigindo erro no excluir livro

--- livro/models.py
+++ livro/models.py
@@ -41,7 +41,7 @@ class Emprestimos(models.Model):
     nome_emprestado_anonimo = models.CharField(max_length=30, blank=True, null=True)
     data_emprestimo = models.DateTimeField(default=datetime.datetime.now())
     data_devolucao = models.DateTimeField(blank=True, null=True)
-    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+    livro = models.ForeignKey(Livros, on_delete=models.SET_NULL, null=True)
     avaliacao = models.CharField(max_length=1, choices=choices, blank=True, null=True)
 
     def __str__(self):
